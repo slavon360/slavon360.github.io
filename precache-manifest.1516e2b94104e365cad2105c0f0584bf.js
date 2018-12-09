e9cf6e506565300327fd7d784e752ea43d1d87f7
slavon360 <slavon360@gmail.com>
2018-12-09 22:26:58 +0200

    build project

---
+++ precache-manifest.1516e2b94104e365cad2105c0f0584bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb5a9198007ecf96b2a3",
+    "url": "/static/js/main.eb5a9198.chunk.js"
+  },
+  {
+    "revision": "fb8933ee84424b24f5ef",
+    "url": "/static/js/1.fb8933ee.chunk.js"
+  },
+  {
+    "revision": "eb5a9198007ecf96b2a3",
+    "url": "/static/css/main.9e96abd2.chunk.css"
+  },
+  {
+    "revision": "9ec910f3aed8083174ea621a9dd063b0",
+    "url": "/index.html"
+  }
+];
